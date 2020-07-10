b556fd575fc399a7834201affc86b38f30819fde
DauletY <erekesh@outlook.com>
2020-07-10 10:12:28 +0500

    Time demo

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using SM = System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    private const string Format = "dd/MM/yyyy";
+    public Text _now = null;
+    public Text _time = null;
+
+    private void OnEnable() => Camera.main.backgroundColor = Color.HSVToRGB(Random.Range(0, 1f), 1.3f, 0.1f);
+    void Start() => DateNow();
+    private void Update() => StartTime();
+    public void DateNow() => _now.text = SM.DateTime.Now.ToString(Format);
+    public void StartTime() => _time.text = string.Format("{0:HH:mm:ss tt}", SM.DateTime.Now);
+
+}
+
